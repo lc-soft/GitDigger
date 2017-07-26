9aaa47f3d33ffe166815393e15e8ed8b5983f1db
Liu Chao <lc-soft@live.cn>
2017-07-26 22:39:05 +0800

    Add compass and bootstrap.css submodule

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../static")
 config.init_app(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
