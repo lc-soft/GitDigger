e981d1c6f069e078e3fb34fce462cc3eafe146c6
Liu Chao <lc-soft@live.cn>
2017-07-29 17:49:07 +0800

    Remove config comments

--- config/__init__.py
+++ config/__init__.py
@@ -4,7 +4,5 @@ from config import database
 def init_app(app):
     github.init_app(app)
     database.init_app(app)
-    # setup secret key 
     app.config['SECRET_KEY'] = '[lc-soft.io, gitdigger.com]'
-    # setup github-flask
     app.config['TEMPLATES_AUTO_RELOAD'] = True
