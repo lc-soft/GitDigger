d1b6b91410093fe242beda2357c6ea81957963b7
Liu Chao <lc-soft@live.cn>
2017-07-31 23:22:30 +0800

    Add issue list to index.html

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -1,8 +1,10 @@
 from app import app, login_manager
 from app.models.topic import Topic
+from app.models.issue import Issue
 from flask import render_template
 
 @app.route('/')
 def index():
     topics = Topic.query.limit(20).all()
-    return render_template('index.html', topics=topics)
+    issues = Issue.query.all()
+    return render_template('index.html', topics=topics, issues=issues)
