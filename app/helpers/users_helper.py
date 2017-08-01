16c0f37f16f09f1a003956b07f45efcde1ac042f
Liu Chao <lc-soft@live.cn>
2017-08-01 23:06:54 +0800

    Add user avatar to issue list

--- app/helpers/users_helper.py
+++ app/helpers/users_helper.py
@@ -1,3 +1,11 @@
+from app import app
+from flask import Markup
+
 class UsersHelper(object):
     def __init__(self, app):
         self.app = app
+
+@app.template_global()
+def user_avatar_tag(user):
+    return  Markup('<img class="user-avatar" alt="%s" src="%s">' %
+                   (user.username, user.avatar_url))
