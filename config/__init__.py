bec6e2a3ed3e9392cac1696d18e9209f0e44419c
Liu Chao <lc-soft@live.cn>
2017-08-08 21:41:46 +0800

    Update config, use from_object() load config

--- config/__init__.py
+++ config/__init__.py
@@ -1,8 +1,7 @@
-from config import github
-from config import database
+from config import github, database
 
 def init_app(app):
-    github.init_app(app)
-    database.init_app(app)
+    app.config.from_object(github)
+    app.config.from_object(database)
     app.config['SECRET_KEY'] = '[lc-soft.io, gitdigger.com]'
     app.config['TEMPLATES_AUTO_RELOAD'] = True
