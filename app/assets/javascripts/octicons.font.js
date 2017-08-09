629918841f3adf34b99320d9e3fcec59a1aa9cc6
Liu Chao <lc-soft@live.cn>
2017-08-09 23:13:48 +0800

    Update octicons.font.js

--- app/assets/javascripts/octicons.font.js
+++ app/assets/javascripts/octicons.font.js
@@ -3,8 +3,11 @@ module.exports = {
     '../../../node_modules/octicons/build/svg/*.svg'
   ],
   fontName: 'octicons',
-  classPrefix: 'octicon-',
-  baseSelector: '.octicon',
+  templateOptions: {
+    src: 'url("/static/fonts/octicons.eot?#iefix") format("embedded-opentype"), url("/static/fonts/octicons.woff") format("woff"), url("/static/fonts/octicons.woff2") format("woff2"), url("/static/fonts/octicons.ttf") format("truetype"), url("/static/fonts/octicons.svg#octicons") format("svg")',
+    classPrefix: 'octicon-',
+    baseSelector: '.octicon'
+  },
   types: ['eot', 'woff', 'woff2', 'ttf', 'svg'],
   fileName: '../fonts/[fontname].[ext]'
 };
