43dc9be503bfb24101bc7bd75399a7ac9d4ad041
Liu Chao <lc-soft@live.cn>
2017-08-11 21:02:32 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-pip install flask flask-script psycopg2 flask-sqlalchemy flask-migrate github-flask flask-login flask-wtf flask_restful psycopg2 gitHub-webhook celery redis
+pip install flask flask-script psycopg2 flask-sqlalchemy flask-migrate github-flask flask-login flask-wtf flask_restful gitHub-webhook celery redis
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+apt-get install python-dev postgresql postgresql-server-dev-all redis-server nodejs
