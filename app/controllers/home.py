3ba8450b5f91aedef0e7ad6beb7c304d984c30fe
Liu Chao <lc-soft@live.cn>
2017-08-13 18:19:36 +0800

    Add recent stories query

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -3,7 +3,7 @@ from app.models.topic import Topic
 from app.models.issue import Issue
 from app.models.voter import Voter
 from app.models.user import User
-from flask import render_template
+from flask import render_template, request
 from flask_login import current_user
 
 @app.route('/')
@@ -12,15 +12,21 @@ def index():
         Topic.group.desc(),
         Topic.issues_count.desc()
     ).limit(10).all()
+    sort = request.args.get('sort', 'top')
     user_id = current_user.id if current_user.is_authenticated else 0
     terms = db.and_(Voter.target_id==Issue.id, Voter.user_id==user_id)
     case = db.case([(Voter.user_id==user_id, True)], else_=False)
     query = db.session.query(Issue, case.label('has_voted'))
-    query = query.order_by(Issue.score.desc(), Issue.created_at.desc())
+    if sort == 'top':
+        query = query.order_by(Issue.score.desc(), Issue.created_at.desc())
+    else:
+        query = query.order_by(Issue.created_at.desc())
     query = query.outerjoin(Voter, terms)
     feeds = query.all()
     ctx = {
+        'feeds': feeds,
+        'topics': topics,
         'navbar_active': 'stories',
-        'feeds_sort_active': 'top'
+        'feeds_sort_active': sort
     }
-    return render_template('index.html', topics=topics, feeds=feeds, **ctx)
+    return render_template('index.html', **ctx)
