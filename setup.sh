51fc4ec86d57d2be25cdc73c91603800d85e467a
Liu Chao <lc-soft@live.cn>
2018-05-27 11:24:47 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install build-essential libffi-dev python-dev postgresql postgresql-server-dev-all redis-server nodejs
+echo "install dependencies ..."
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y build-essential libffi-dev python-dev postgresql postgresql-server-dev-all nginx redis-server python-pip nodejs
 sudo pip install setuptools pipenv uwsgi
 pipenv install
 
 mkdir log
+mkdir tmp
 mkdir db/migrate/versions
+
+echo "create default config file ..."
+cp config/github.py.example config/github.py
+cp config/database.py.postgresql.example config/database.py
+
+echo "create database ..."
+sudo -u postgres createuser gitdigger -P
+sudo -u postgres createdb -O gitdigger gitdigger_development
