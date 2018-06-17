2c79a06936590d926cdc22ceb170a548d79b6fc8
Liu Chao <lc-soft@live.cn>
2018-06-17 10:38:36 +0800

    feat: use avinit to render the default avatar

--- app/helpers/users_helper.py
+++ app/helpers/users_helper.py
@@ -1,3 +1,4 @@
+import avinit
 from app import app
 from flask import Markup
 
@@ -7,8 +8,11 @@ class UsersHelper(object):
 
 @app.template_global()
 def user_avatar_tag(user):
+    avatar_url = user.avatar_url
+    if not user.avatar_url:
+        avatar_url = avinit.get_avatar_data_url(user.name)
     return Markup('<img class="user-avatar" title="%s" alt="%s" src="%s">' %
-                   (user.username, user.username, user.avatar_url))
+                   (user.username, user.username, avatar_url))
 
 @app.template_global()
 def user_is_following_topic(user, topic_name):
