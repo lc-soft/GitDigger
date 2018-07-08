6659be22f053248021aa7bb30ff0121382a1a887
Liu Chao <lc-soft@live.cn>
2018-07-08 17:46:25 +0800

    refactor: use app_data() to provide data to front end

--- app/helpers/application_helper.py
+++ app/helpers/application_helper.py
@@ -1,29 +1,55 @@
 import flask
 from app import app
 from datetime import datetime
+from flask import request, url_for
 from flask_login import current_user
 
 def flash(message, category='info'):
     return flask.flash(message, category)
 
 @app.template_global()
-def rating_text_tag(rating):
-    if rating > 8:
-        level = 4
-        text = 'so difficult'
-    elif rating > 7:
-        level = 3
-        text = 'difficult'
-    elif rating > 4:
-        level = 2
-        text = 'normal'
-    elif rating > 2:
-        level = 1
-        text = 'easy'
+def app_data():
+    data = {
+        'endpoint': request.endpoint,
+        'api': {
+            'issues': url_for('api.issues'),
+            'snippet_voters': url_for('api.snippet_voters', id=':id', username=':username'),
+        }
+    }
+    if current_user.is_authenticated:
+        data['user'] = {
+            'login': current_user.username,
+            'name': current_user.name,
+            'github_username': current_user.github_username
+        }
     else:
-        level = 0
-        text = 'so easy'
-    html = '<span class="rating-text rating-text-{0}">{1}</span>'
+        data['user'] = {
+            'login': None,
+            'name': 'unknown'
+        }
+    return data
+
+@app.template_global()
+def rating_options():
+    return [
+        (0, 0, 'so easy'),
+        (2, 4, 'easy'),
+        (4, 6, 'normal'),
+        (6, 8, 'difficult'),
+        (8, 10, 'so difficult')
+    ]
+
+@app.template_global()
+def rating_text_tag(rating):
+    level = 0
+    options = rating_options()
+    text = options[0][2]
+    for i in range(len(options) - 1, 0, -1):
+        if rating > options[i][0]:
+            level = i
+            text = options[i][2]
+            break
+    html = '<span class="rating-text js-rating-text" data-value="{0}">{1}</span>'
     return flask.Markup(html.format(level, text))
 
 @app.template_global()
