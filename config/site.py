575434056e5ecc926567c1a1bfbcef02fb7e1089
Liu Chao <lc-soft@live.cn>
2018-07-08 22:03:44 +0800

    feat(snippets): add side nav

--- config/site.py
+++ config/site.py
@@ -4,5 +4,6 @@ config = {
     'owner_url': 'https://lc-soft.io',
     'github_url': 'https://github.com/lc-soft/GitDigger',
     'blog_url': 'http://blog.lc-soft.io/posts/gitdigger-0.1.0.html',
+    'snippet_help_url': 'https://github.com/lc-soft/GitDigger/blob/master/scripts/comment_digger.py',
     'domain': 'gitdigger.com'
 }
